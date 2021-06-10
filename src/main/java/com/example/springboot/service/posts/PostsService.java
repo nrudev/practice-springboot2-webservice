f7b5fc57289a80a2f742e88f0de490e9d92948bb
hskoo <nrudev812@gmail.com>
2021-06-10 19:32:12 +0900

    4.3 ~ 4.4

--- src/main/java/com/example/springboot/service/posts/PostsService.java
+++ src/main/java/com/example/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.example.springboot.service.posts;
 
 import com.example.springboot.domain.posts.Posts;
 import com.example.springboot.domain.posts.PostsRepository;
+import com.example.springboot.web.dto.PostsListResponseDto;
 import com.example.springboot.web.dto.PostsResponseDto;
 import com.example.springboot.web.dto.PostsSaveRequestDto;
 import com.example.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 // Bean을 주입받는 방식(@Autowired, setter, 생성자) 중 가장 권장되는 방식은 생성자로 주입받는 방식.
 @RequiredArgsConstructor // final이 선언된 모든 필드를 인자값으로 하는 생성자를 대신 생성.
@@ -36,5 +40,14 @@ public class PostsService {
                 new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+
+
     }
 }
